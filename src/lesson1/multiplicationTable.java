b6b54cc7edda8850a21fc88b89e023701449388e
lilit.dashyan <lilit.dashyan@webbfontaine.com>
2020-09-09 01:19:35 +0400

    calculator

--- src/lesson1/multiplicationTable.java
+++ src/lesson1/multiplicationTable.java
@@ -6,11 +6,10 @@ public class multiplicationTable {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("enter the number: ");
-        int n=input.nextInt();
-        for(int i=1; i <= 10; i++)
-        {
-            System.out.println(n+" * "+i+" = "+n*i);
+        int n = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
         }
     }
 
-    }
+}
